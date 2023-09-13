7943f0c9c3edde5f559cb05a085116b302764e3f
ravikumar7007 <ravikumarvelu2000@gmail.com>
2023-09-13 22:37:03 +0530

    State management for login and register

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,36 +1,57 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+  function handleChange(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!password) {
+      console.log("Incorrect Password");
+    } else {
+      console.log(formData);
+    }
+  }
   return (
-      <section class="container">
-      <div class="alert alert-danger">
-        Invalid credentials
-      </div>
-      <h1 class="large text-primary">Sign In</h1>
-      <p class="lead"><i class="fas fa-user"></i> Sign into Your Account</p>
-      <form class="form" action="dashboard.html">
-        <div class="form-group">
+    <section className="container">
+      <div className="alert alert-danger">Invalid credentials</div>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign into Your Account
+      </p>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-group">
           <input
             type="email"
             placeholder="Email Address"
             name="email"
+            value={email}
+            onChange={handleChange}
             required
           />
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <input
             type="password"
             placeholder="Password"
+            value={password}
+            onChange={handleChange}
             name="password"
           />
         </div>
-        <input type="submit" class="btn btn-primary" value="Login" />
+        <input type="submit" className="btn btn-primary" value="Login" />
       </form>
-      <p class="my-1">
-        Don't have an account? <a href="register.html">Sign Up</a>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
       </p>
     </section>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,43 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Register() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+  const { name, email, password, password2 } = formData;
+  function handleChange(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log("Incorrect Password");
+    } else {
+      console.log(formData);
+    }
+  }
   return (
-    <section class="container">
-      <h1 class="large text-primary">Sign Up</h1>
-      <p class="lead">
-        <i class="fas fa-user"></i> Create Your Account
+    <section className="container">
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Create Your Account
       </p>
-      <form class="form" action="create-profile.html">
-        <div class="form-group">
-          <input type="text" placeholder="Name" name="name" required />
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={handleChange}
+            required
+          />
         </div>
-        <div class="form-group">
-          <input type="email" placeholder="Email Address" name="email" />
-          <small class="form-text">
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={handleChange}
+          />
+          <small className="form-text">
             This site uses Gravatar so if you want a profile image, use a
             Gravatar email
           </small>
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <input
             type="password"
             placeholder="Password"
             name="password"
             minLength="6"
+            value={password}
+            onChange={handleChange}
           />
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <input
             type="password"
             placeholder="Confirm Password"
             name="password2"
             minLength="6"
+            value={password2}
+            onChange={handleChange}
           />
         </div>
-        <input type="submit" class="btn btn-primary" value="Register" />
+        <input type="submit" className="btn btn-primary" value="Register" />
       </form>
-      <p class="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+      <p className="my-1">
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </section>
   );
